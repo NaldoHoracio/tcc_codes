32a4b0df1b4ebdaf43681da4837fe27f9d78ea1c
NaldoHorácio <edvonaldohs@ic.ufal.br>
2020-02-19 21:39:03 -0300

    Script com a leitura da coluna no valor desejado.

--- read_csv_files/read_csv.py
+++ read_csv_files/read_csv.py
@@ -4,7 +4,12 @@ import numpy as np
 import pandas as pd
 
 
-data = pd.read_csv('DM_CURSO.csv', sep='|')
+path_file = '/home/horacio/Documentos/UFAL/TCC/tcc_data/microdados_censo_superior_2016/DADOS/DM_CURSO.csv'
+column1 = ['NO_MUNICIPIO_CURSO']
+
+data = pd.read_csv(path_file, sep = '|')
+uf_al = data[data['SGL_UF_CURSO'] == 'AL']
+#data = pd.read_csv(path_file, sep = '|', skipinitialspace=True, usecols=column1)
 
 
 
