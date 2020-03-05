cc98da820d8f105d1490ac77b7f1d7f8ae36dc56
NaldoHorácio <edvonaldohs@ic.ufal.br>
2020-03-04 22:06:18 -0300

    Atualizando código.

--- read_csv_files/read_csv.py
+++ read_csv_files/read_csv.py
@@ -4,16 +4,22 @@ import numpy as np
 import pandas as pd
 
 
-path_file_ufal = '/home/horacio/Documentos/UFAL/TCC/tcc_data/UFAL.csv'
-path_file_brazil = '/home/horacio/Documentos/UFAL/TCC/tcc_data/Random.csv'
+path_file_ufal = '/home/horacio/Documentos/UFAL/TCC/tcc_codes/tcc_data/UFAL.csv'
+path_file_brazil = '/home/horacio/Documentos/UFAL/TCC/tcc_codes/tcc_data/Random.csv'
 
-data_ufal = pd.read_csv(path_file_ufal, sep = ',')
 data_brazil = pd.read_csv(path_file_brazil, sep = ',')
-
-#data[data['SGL_UF_CURSO'] == 'AL']
-#without_al = data[data['SGL_UF_CURSO'] != 'AL']
-#data_random = without_al.sample(10000)
+#data_ufal = pd.read_csv(path_file_ufal, sep = ',')
 
 
+
+#for col in data_brazil.columns:
+#    print(col)
+    
+    
+#def main():
+#    read_file()
+
+
+#main()
 
     
