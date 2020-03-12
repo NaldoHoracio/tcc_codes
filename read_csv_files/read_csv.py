1871cce52f455f2cc643505344d1c3429d2df279
NaldoHorácio <edvonaldohs@ic.ufal.br>
2020-03-12 11:03:24 -0300

    Dados ENADE UFAL 2017.

--- read_csv_files/read_csv.py
+++ read_csv_files/read_csv.py
@@ -1,25 +1,27 @@
 import os
 import csv
+import random
 import numpy as np
 import pandas as pd
 
 
-path_file_ufal = '/home/horacio/Documentos/UFAL/TCC/tcc_codes/tcc_data/UFAL.csv'
-path_file_brazil = '/home/horacio/Documentos/UFAL/TCC/tcc_codes/tcc_data/Random.csv'
+#path_file_al = '/home/horacio/Documentos/UFAL/TCC/tcc_codes/tcc_data/UFAL.csv'
+#path_file_brazil = '/home/horacio/Documentos/UFAL/TCC/tcc_codes/tcc_data/Random.csv'
+path = '/home/horacio/Documentos/UFAL/ENADE/microdados_Enade_2017_portal_2018.10.09/3.DADOS/MICRODADOS_ENADE_2017.txt'
 
-data_brazil = pd.read_csv(path_file_brazil, sep = ',')
-#data_ufal = pd.read_csv(path_file_ufal, sep = ',')
+#data_brazil = pd.read_csv(path_file_brazil, sep = ',')
+#data_al = pd.read_csv(path_file_al, sep = ',')
+data = pd.read_csv(path, sep = ';')
+data_al = data[data['QE_I16'] == 27]
+data_al_save = data_al.to_csv('UFAL.csv')
+#data_br = data[data['QE_I16'] != 27]
 
 
-
-#for col in data_brazil.columns:
+#for col in data_al.columns:
 #    print(col)
     
     
 #def main():
-#    read_file()
-
-
-#main()
+#    read_file()#main()
 
     
