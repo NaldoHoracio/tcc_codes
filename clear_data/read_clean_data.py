77d0b6c22de2a29c54b0d60c0d074e75fc43ba17
NaldoHorácio <edvonaldohs@ic.ufal.br>
2020-03-28 18:23:44 -0300

    Dados convertido em float. Próximo passo: somar os valores da lista.

--- clear_data/read_clean_data.py
+++ clear_data/read_clean_data.py
@@ -11,7 +11,6 @@ import random
 import numpy as np
 import pandas as pd
 
-
 path_al = '/home/horacio/Documentos/UFAL/TCC/tcc_codes/clear_data/AL_data_clean.csv'
 path_br = '/home/horacio/Documentos/UFAL/TCC/tcc_codes/clear_data/Brazil_Random_data_clean.csv'
 
@@ -27,10 +26,20 @@ for idx in data_al.index:
         aux_data_al.append(element.replace(',','.'))
     elif isinstance(element, str) == False:
         aux_data_al.append(element)
-        
-# Transformando a string em float
-for idx in aux_data_al:
+
+
+for idx in range(len(aux_data_al)):
+    element = aux_data_al[idx]
     if isinstance(aux_data_al[idx], str) == True:
-        print("String")
+        aux_data_al[idx] = float(element)
     elif isinstance(aux_data_al[idx], str) == False:
         print("Float")
+
+sum_list = 0.0
+for idx in range(len(aux_data_al)):
+    if aux_data_al[idx] == 'nan':
+        sum_list = sum_list
+    elif aux_data_al[idx] != 'nan':
+        sum_list += aux_data_al[idx]
+        
+print("Soma da lista ", sum_list)
