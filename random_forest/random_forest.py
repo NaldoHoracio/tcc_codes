6bb1c1925c86a6adc0f5f049a8d409a3fbfd921c
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2020-04-24 22:55:06 -0300

    Update random_forest.py

--- random_forest/random_forest.py
+++ random_forest/random_forest.py
@@ -13,16 +13,71 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.datasets import make_regression
+path = 'G:/Meu Drive/UFAL/2020.1/Aprendizado de Máquina/Databases/temps.csv'
 
-X, y = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
+features = pd.read_csv(path)
 
-regr = RandomForestRegressor(max_depth=2, random_state=0)
+anomalies = features.describe()
 
-regr.fit(X,y,sample_weight=None)
+# Janeiro
+jan = features[features['month'] == 1]
+jan_average = jan['average']
+print('Average Jan %.2f' %(jan_average.sum()/jan_average.size))
 
-decision_path_RF = regr.decision_path(X)
+# Fevereiro
+feb = features[features['month'] == 2]
+feb_average = feb['average']
+print('Average Feb %.2f' %(feb_average.sum()/feb_average.size))
 
-print("Caminho de decisão Random Forest: ", decision_path_RF)
+# Março
+mar = features[features['month'] == 3]
+mar_average = mar['average']
+print('Average Mar %.2f' %(mar_average.sum()/mar_average.size))
+
+# Abril
+abr = features[features['month'] == 4]
+abr_average = abr['average']
+print('Average Abr %.2f' %(abr_average.sum()/abr_average.size))
+
+# Maio
+may = features[features['month'] == 5]
+may_average = may['average']
+print('Average May %.2f' %(may_average.sum()/may_average.size))
+
+# Junho
+jun = features[features['month'] == 6]
+jun_average = jun['average']
+print('Average Jun %.2f' %(jun_average.sum()/jun_average.size))
+
+# Julho
+jul = features[features['month'] == 7]
+jul_average = jul['average']
+print('Average Jul %.2f' %(jul_average.sum()/jul_average.size))
+
+# Agosto
+aug = features[features['month'] == 8]
+aug_average = aug['average']
+print('Average Aug %.2f' %(aug_average.sum()/aug_average.size))
+
+# Setembro
+sept = features[features['month'] == 9]
+sept_average = sept['average']
+print('Average Sept %.2f' %(sept_average.sum()/sept_average.size))
+
+# Outubro
+octb = features[features['month'] == 10]
+octb_average = octb['average']
+print('Average Octb %.2f' %(octb_average.sum()/octb_average.size))
+
+# Novembro
+nov = features[features['month'] == 11]
+nov_average = nov['average']
+print('Average Nov %.2f' %(nov_average.sum()/nov_average.size))
+
+# Dezembro
+dec = features[features['month'] == 12]
+dec_average = dec['average']
+print('Average Dec %.2f' %(dec_average.sum()/dec_average.size))
+
+        
 
