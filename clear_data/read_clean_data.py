e44e7adee02c7acfb59da8a3ac400983681776ff
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2020-05-01 01:24:23 -0300

    Teste para substituir o nan pela mediana.

--- clear_data/read_clean_data.py
+++ clear_data/read_clean_data.py
@@ -46,4 +46,18 @@ for idx in range(len(aux_data_al)):
 
 print("Soma da lista: ", s_list)
 print("Nan number: ", cont_nan)
-        
+
+#%%
+# importing pandas as pd 
+import pandas as pd 
+  
+# Creating the dataframe  
+df = pd.DataFrame({"A":[12, 4, 5, None, 1], 
+                   "B":[7, 2, 54, 3, None],  
+                   "C":[20, 16, 11, 3, 8],  
+                   "D":[14, 3, None, 2, 6]})
+
+print(df['A'].median())
+print(df['B'].median())
+print(df['C'].median())
+print(df['D'].median())
--- random_forest/random_forest.py
+++ random_forest/random_forest.py
@@ -22,6 +22,12 @@ features_al = pd.read_csv(path_al)
 
 del features_al['Unnamed: 0']
 
+
+# Choosing only the columns of interest
+features_al = features_al.loc[:,'NT_GER':'QE_I26']
+features_al = features_al.drop(features_al.loc[:, 'CO_RS_I1':'CO_RS_I9'].columns, axis=1)
+
+#%% Observando os dados
 print('O formato dos dados é: ', features_al.shape)
 
 describe_al = features_al.describe()
@@ -29,7 +35,42 @@ describe_al = features_al.describe()
 print('Descrição para as colunas: ', describe_al)
 print(describe_al.columns)
 
-#%% Choosing only the columns of interest
-features_al = features_al.loc[:,'NT_GER':'QE_I26']
-features_al = features_al.drop(features_al.loc[:, 'CO_RS_I1':'CO_RS_I9'].columns, axis=1)
+#%% Números que são strings por float
+# Colunas NT_GER a NT_DIS_FG ^ NT_CE a NT_DIS_CE
+features_al['NT_GER'] = features_al['NT_GER'].str.replace(',','.')
+features_al['NT_GER'] = features_al['NT_GER'].astype(float)
 
+features_al['NT_FG'] = features_al['NT_FG'].str.replace(',','.')
+features_al['NT_FG'] = features_al['NT_FG'].astype(float)
+
+features_al['NT_OBJ_FG'] = features_al['NT_OBJ_FG'].str.replace(',','.')
+features_al['NT_OBJ_FG'] = features_al['NT_OBJ_FG'].astype(float)
+
+features_al['NT_DIS_FG'] = features_al['NT_DIS_FG'].str.replace(',','.')
+features_al['NT_DIS_FG'] = features_al['NT_DIS_FG'].astype(float)
+
+features_al['NT_CE'] = features_al['NT_CE'].str.replace(',','.')
+features_al['NT_CE'] = features_al['NT_CE'].astype(float)
+
+features_al['NT_OBJ_CE'] = features_al['NT_OBJ_CE'].str.replace(',','.')
+features_al['NT_OBJ_CE'] = features_al['NT_OBJ_CE'].astype(float)
+
+features_al['NT_DIS_CE'] = features_al['NT_DIS_CE'].str.replace(',','.')
+features_al['NT_DIS_CE'] = features_al['NT_DIS_CE'].astype(float)
+
+#%% Trocando nan pela mediana
+
+for idx in range()
+
+#%%
+features_al['NT_FG'] = features_al['NT_FG'].fillna(features_al['NT_FG'].median())
+
+features_al['NT_OBJ_FG'] = features_al['NT_OBJ_FG'].fillna(features_al['NT_OBJ_FG'].median())
+
+features_al['NT_DIS_FG'] = features_al['NT_DIS_FG'].fillna(features_al['NT_DIS_FG'].median())
+
+features_al['NT_CE'] = features_al['NT_CE'].fillna(features_al['NT_CE'].median())
+
+features_al['NT_OBJ_CE'] = features_al['NT_OBJ_CE'].fillna(features_al['NT_OBJ_CE'].median())
+
+features_al['NT_DIS_CE'] = features_al['NT_DIS_CE'].fillna(features_al['NT_DIS_CE'].median())
