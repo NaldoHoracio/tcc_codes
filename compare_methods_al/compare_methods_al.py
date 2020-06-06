0c949fc474c0b48b5f947f2bea03656dc38c4853
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2020-06-05 22:39:51 -0300

    Método de comparação encontrado. Atualizar os gráficos.

--- compare_methods_al/compare_methods_al.py
+++ compare_methods_al/compare_methods_al.py
@@ -245,11 +245,11 @@ I25_AL_DT = importance_fields_al_dt_t[140:148]; I26_AL_DT = importance_fields_al
 #%% Visualization of Variable Importances
 # QE_I01
 fig1 = plt.figure();
-fig1.suptitle('Método: Árvore de Decisão', fontsize=10, fontweight='bold')
 ax1 = fig1.add_axes([0,0,1,1]);
 x1 = ['Solteiro', 'Casado (a)', 'Separado', 'Viúvo', 'Outro'];
 y1 = [I01_AL[0],I01_AL[1],I01_AL[2],I01_AL[3],I01_AL[4]];
 ax1.bar(x1,y1);
+plt.xticks(x1,rotation=90,fontsize=8);
 plt.ylabel('Importância'); 
 plt.xlabel('Variável');
 plt.title('Estado civil');
@@ -660,3 +660,40 @@ plt.ylabel('Importância');
 plt.xlabel('Variável');
 plt.title('QE_I14 a QE_I26');
 plt.savefig('13_26_AL_DT.png', dpi=450, bbox_inches='tight');
+
+#%%
+# libraries
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# set width of bar
+bar_width = 0.1
+ 
+# set height of bar
+bars1 = [12, 30, 1, 8, 22]
+bars2 = [28, 6, 16, 5, 10]
+bars3 = [29, 3, 24, 25, 17]
+bars4 = [44.3, 44.5, 80, 150, 15]
+ 
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + bar_width for x in r1]
+r3 = [x + bar_width for x in r2]
+r4 = [x + bar_width for x in r3]
+ 
+# Make the plot
+plt.bar(r1, bars1, color='red', width=bar_width, edgecolor='white', label='var1')
+plt.bar(r2, bars2, color='green', width=bar_width, edgecolor='white', label='var2')
+plt.bar(r3, bars3, color='blue', width=bar_width, edgecolor='white', label='var3')
+plt.bar(r4, bars4, color='yellow', width=bar_width, edgecolor='white', label='var4')
+ 
+# Add xticks on the middle of the group bars
+plt.xlabel('group', fontweight='bold')
+plt.xticks([r + bar_width for r in range(len(bars1))], ['A', 'B', 'C', 'D', 'E'],rotation=90, fontsize=8)
+ 
+# Create legend & Show graphic
+plt.ylabel('Importância'); 
+plt.xlabel('Variável');
+plt.title('QE_I14 a QE_I26');
+plt.legend()
+plt.show()
